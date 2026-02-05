e7b98076003734d086d61a2d890a9f742aefc526
Chamodya Ganepola <102275627+ChamodyaGanepola@users.noreply.github.com>
2026-02-05 12:18:29 +0530

    Add comments for MySQL credentials in db.php
    Added comments for local development and deployment.

--- db.php
+++ db.php
@@ -1,12 +1,14 @@
 <?php
+// Local development (XAMPP) MySQL credentials
 $host = "localhost";
 $user = "root";
 $pass = "";
 $dbname = "personal_tracker";
-
+// Note: When deploying to InfinityFree, used the credentials provided in the folder structure
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
